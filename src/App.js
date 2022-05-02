77aafe87ace09033fef576a93f3bd350f43f01c2
oshknay5 <oshknay82@gmail.com>
2022-05-02 18:34:32 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
+import { dataBook } from './dataBook';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+  function App () {
+ 
+ const [books, setBooks]=useState(data)
+ const [ showText, setShowText] = useState (false)
+
+//  для слайдов
+ const [newBook, setNewBook] =useState(0);
+ const {nameBook, authorBook, imageBook} = dataBook[newBook];
+
+ const previousBook = () =>{
+setNewBook((book=>{
+  book--;
+  if ( book < 0) {
+     return dataBook.length-1;
+  }
+     return book;
+}))
+ }
+
+ const nextBook = () => {
+   setNewBook ( (book => {
+     book ++;
+     if (book > dataBook.length-1){
+     book = 0;
+     }
+     return book;
+   }))
+ }
+
+// ---------
+
+ const removeBook = (id) => {
+   const newBooks = books.filter((book)=>
+   book.id !==id);
+   setBooks(newBooks)
+ }
+
+
+  const showTextClick = (book)=>{
+    book.showMore = !book.showMore;
+    setShowText(!showText)
+  }
+
+    return ( <div>
+      <div className="container">
+      <h1> {books.length} books must-read  </h1>
+      </div>
+{books.map (( book =>{
+  const {id, name, description, author, image, showMore } = book;
+
+  return( 
+    <div key={id}> 
+<div className="top container">
+     <h2> {name} </h2>
+      </div>
+<div className="container">
+  <h2>{author} </h2>
+</div>
+
+<div className="container">
+  <img src={image} width='300px' alt='book'/>
+</div>
+
+<div className="container">
+ <p className="par"> {showMore ? description : description.substring(0, 220) + '...'} <button className='btnMore' onClick={()=> showTextClick(book)}>{showMore ? '':'Подробнее'} </button> </p>
+</div>  
+
+<div className="container">
+<button className='btn' onClick={() => removeBook(id)}> Удалить </button>
+</div>  
+ </div>
+  )
+}))
 }
+
+<div className="container">
+<button className='btn' onClick={() =>setBooks([])}> Удалить все</button>
+</div> 
+
+{/* слайды */}
+<div className="container">
+      <h1> Рекомендуем почитать</h1> 
+      </div>
+<div className="container">
+     <h2> {nameBook} </h2>
+      </div>
+<div className="container">
+  <h2>{authorBook} </h2>
+</div>
+
+<div className="container">
+  <img src={imageBook} width='250px' height='350px'  alt='newbook'/>
+</div> 
+
+<div className="btnSlade container">
+  <button onClick={previousBook}>Previous</button>
+  <button onClick={nextBook}>Next </button>
+</div> 
+
+</div>    
+);
+}      
 
 export default App;
---
+++ src/dataBook.js
@@ -0,0 +1,37 @@
+export const dataBook = [
+    {
+        idBook: 1,
+        nameBook: 'Алхимик',
+        authorBook: ' Пауло Коэльо',
+        imageBook: 'https://books.google.by/books/publisher/content?id=1D5mDwAAQBAJ&hl=ru&pg=PP1&img=1&zoom=3&sig=ACfU3U2r5-q_xlxpRoJmM6jvM6ESsnzX0Q&w=1280'
+
+    },
+    {
+        idBook: 2,
+        nameBook: 'Гордость и предубеждение',
+        authorBook: ' Джейн Остен',
+        imageBook:' https://staticlb.rmr.rocks/uploads/pics/01/54/860.jpg'
+
+    },
+    {
+        idBook: 3,
+        nameBook: 'Парфюмер. История одного убийцы',
+        authorBook: ' Патрик Зюскинд',
+        imageBook: 'https://upload.wikimedia.org/wikipedia/ru/thumb/d/dd/Das-Parfume.jpg/213px-Das-Parfume.jpg'
+
+    },
+    {
+        idBook: 4,
+        nameBook: 'Маленький принц',
+        authorBook: ' Антуан Де Сент-Экзюпери',
+        imageBook:' https://cdn21vek.by/img/galleries/5897/430/eksmo_03087_5f3d3bd69d28d.jpeg'
+
+    },
+    {
+        idBook: 5,
+        nameBook: 'Только для посвященных',
+        authorBook: ' Татьяна Коган',
+        imageBook: 'https://img.7ya.ru/ratings/books/ed6760.jpg'
+
+    }
+]
